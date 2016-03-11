5d03d6320825af54e5ecf17af19f2bfe3758599a
Tom Piarulli <tompiarulli@gmail.com>
2016-03-11 14:10:29 -0500

    HTTP method comments

--- DeepBlueApp/HTTPMethod.swift
+++ DeepBlueApp/HTTPMethod.swift
@@ -8,6 +8,20 @@
 
 import Foundation
 
+/*
+    When configuring an NSURLRequest with Cocoa it takes in
+    the HTTPMethod as a string i.e. "GET". Since using strings
+    as type representations is bad practice we can create
+    an enum with a raw type of String.
+
+    This will allow us to use the safer enum syntax i.e. 
+    `HTTPMethod.GET` while keeping it easily convertible to
+    a String.
+
+    By default in Swift an enum with a String as the raw type
+    will have a raw value of the case name as a String. For
+    example `HTTPMethod.GET.rawValue` will give us "GET".
+*/
 enum HTTPMethod: String {
     case GET
     case POST
